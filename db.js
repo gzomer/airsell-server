c829f20254798a53368ef6fc6c1b653e7a1e986a
Gustavo Zomer <gustavozomer@gmail.com>
2020-07-02 10:57:49 +0100

    Add db

---
+++ db.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+var self = module.exports = {}
+self.models = {}
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  name: String,
+  username: String,
+  password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const EcommerceSchema = new Schema({
+  name: String,
+  description: String,
+  logo: String,
+  theme: String,
+  domain: String,
+  homeDescription: String,
+  homeTitle: String,
+  facebook: String,
+  instagram: String,
+  apiKey: String,
+  airtable: Object,
+  fieldsMapping : Object,
+  data: Object,
+});
+
+self.models.User = mongoose.model('User', UserSchema, 'User');
+self.models.Ecommerce = mongoose.model('Ecommerce', EcommerceSchema, 'Ecommerce');
