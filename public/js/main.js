559ff66e244af23fda8f6cb1605658b5d4cc4542
Gustavo Zomer <gustavozomer@gmail.com>
2020-07-02 10:58:02 +0100

    Add main js

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+function setCookie(key, value, expiry=10) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString()+ ";path=/";
+}
+
+function clearCookies() {
+  document.cookie = '';
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+function eraseCookie(key) {
+    var keyValue = getCookie(key);
+    setCookie(key, keyValue, '-1');
+}
+
+function readCart() {
+  var products = []
+  var currentProducts = getCookie('basket')
+
+  if (currentProducts) {
+    products = JSON.parse(atob(currentProducts))
+  }
+
+  return products
+}
+
+function addToCart(product, quantity, attributes) {
+  var products = readCart()
+  products.push({
+    id: product,
+    quantity: quantity,
+    attributes: attributes
+  })
+
+  setCookie('basket', btoa(JSON.stringify(products)))
+}
+
+function goCheckout() {
+  var STRIPE_KEY = 'pk_test_ZWhwAIbGPDhTQFH2Cff2S0P3';
+
+  stripe = Stripe(STRIPE_KEY);
+
+  $.post('/purchase/session',{
+      successURL : 'http://'+window.location.host + '/order/',
+      cancelURL : window.location.href,
+  }, function(response){
+      stripe.redirectToCheckout({
+        sessionId: response.session
+      }).then(function (result) {
+        console.log(result)
+      });
+  })
+}
+
+$(function() {
+  $("#addToCart").click(function() {
+    var attributes = {}
+    $(".product-attributes").each(function(){
+        attributes[$(this).attr('name')] = $(this).val()
+    })
+
+    var quantity = parseInt($(".product-quantity").val())
+    var product = $(".product-id").val()
+
+    addToCart(product, quantity, attributes)
+
+    bootbox.alert($("h1").text() + " added to cart")
+  })
+
+  $("#emptyCart").click(function() {
+    eraseCookie('basket')
+    window.location.reload()
+  })
+
+  $("#checkout").click(goCheckout)
+})
